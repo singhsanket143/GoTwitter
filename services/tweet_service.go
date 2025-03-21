07cfe5667ee4a602f69bcd9ec70968efddf6cb9d
Sanket Singh <singhsanket143@gmail.com>
2025-03-21 19:24:33 +0530

    Added goroutines to make the addition of tweets go parallel

--- services/tweet_service.go
+++ services/tweet_service.go
@@ -7,6 +7,7 @@ import (
 	"GoTwitter/utils"
 	"context"
 	"fmt"
+	"sync"
 )
 
 type TweetService interface {
@@ -29,43 +30,74 @@ func NewTweetService(
 	return &tweetService{tweetRepository, tagRepository, tweetTagsRepository}
 }
 
+// CreateTweet handles the creation of a new tweet along with its associated hashtags and tweet-tags.
+// It performs the following steps:
+// 1. Parses hashtags from the provided tweet content.
+// 2. Creates the hashtags (tags) in the database using a goroutine.
+// 3. Creates the tweet in the database using another goroutine.
+// 4. Waits for both the hashtag creation and tweet creation to complete.
+// 5. Associates the created hashtags with the tweet by creating tweet-tags in the database.
+//
+// Parameters:
+// - ctx: The context for managing request-scoped values, deadlines, and cancellations.
+// - tweet: A pointer to a CreateTweetDTO object containing the tweet content.
+//
+// Returns:
+// - A pointer to the created Tweet model if successful.
+// - An error if any step in the process fails.
 func (s *tweetService) CreateTweet(ctx context.Context, tweet *dto.CreateTweetDTO) (*models.Tweet, error) {
-
 	tweetContent := tweet.Tweet
 
 	// parse hashtags from tweet content
 	hashtags := utils.ParseHashtags(tweetContent)
-
 	fmt.Println("Hashtags: ", hashtags)
 
-	// create tags
-	tags, err := s.tagRepository.BulkCreate(ctx, hashtags)
+	var (
+		newtweet *models.Tweet
+		tags     []*models.Tag
+		tweetErr error
+		tagsErr  error
+	)
 
-	if err != nil {
-		return nil, err
+	// Use a WaitGroup to wait for both goroutines to complete
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// Create tags in a goroutine
+	go func() {
+		defer wg.Done()
+		tags, tagsErr = s.tagRepository.BulkCreate(ctx, hashtags)
+	}()
+
+	// Create tweet in a goroutine
+	go func() {
+		defer wg.Done()
+		newtweet, tweetErr = s.tweetRepository.Create(ctx, tweet)
+	}()
+
+	// Wait for both goroutines to complete
+	wg.Wait()
+
+	// Check for errors
+	if tagsErr != nil {
+		return nil, tagsErr
 	}
-
-	for _, tag := range tags {
-		fmt.Printf("Tag: %+v\n", tag)
-	}
-
-	newtweet, tweetErr := s.tweetRepository.Create(ctx, tweet)
-
 	if tweetErr != nil {
 		return nil, tweetErr
 	}
 
 	fmt.Printf("New Tweet: %+v\n", newtweet)
+	for _, tag := range tags {
+		fmt.Printf("Tag: %+v\n", tag)
+	}
 
-	// create tweet_tags
+	// Create tweet_tags
 	tagIds := make([]int64, 0)
-
 	for _, tag := range tags {
 		tagIds = append(tagIds, tag.Id)
 	}
 
 	_, tweetTagsErr := s.tweetTagsRepository.BulkCreate(ctx, tagIds, newtweet.Id)
-
 	if tweetTagsErr != nil {
 		return nil, tweetTagsErr
 	}
