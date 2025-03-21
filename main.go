0414e0ae221f3efc51f35043b7b15f72e02c8ac9
Sanket Singh <singhsanket143@gmail.com>
2025-03-21 16:24:09 +0530

    Added graph visualisation for uber fx DI

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 	"GoTwitter/handlers"
 	"GoTwitter/router"
 	"GoTwitter/services"
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 )
 
 func main() {
@@ -19,12 +24,17 @@ func main() {
 	config.LoadEnv()
 
 	app := fx.New(
+
+		fx.WithLogger(func() fxevent.Logger {
+			return &fxevent.ConsoleLogger{W: os.Stdout}
+		}),
+
 		fx.Provide(
 			dbConfig.SetupNewDbConn,
 
 			fx.Annotate(
 				db.NewTweetsStore,
-				fx.As(new(db.TweetsRepository)), // 👈 Important line
+				fx.As(new(db.TweetsRepository)),
 			),
 
 			db.NewUsersStore,
@@ -34,8 +44,42 @@ func main() {
 			router.NewTweetRouter,
 			router.Mount,
 		),
-		fx.Invoke(func(r *chi.Mux) {
-			http.ListenAndServe(":8080", r)
+
+		fx.Invoke(func(dot fx.DotGraph) {
+			file, err := os.Create("fx-graph.dot") // brew install graphviz && dot -Tpng fx-graph.dot -o fx-graph.png
+
+			if err != nil {
+				log.Println("Failed to create graph file:", err)
+				return
+			}
+			defer file.Close()
+
+			_, err = fmt.Fprintf(file, "%s", dot)
+			if err != nil {
+				log.Println("Failed to write DOT graph:", err)
+			} else {
+				log.Println("DOT graph written to fx-graph.dot")
+			}
+		}),
+
+		// Start HTTP server
+		fx.Invoke(func(lc fx.Lifecycle, r *chi.Mux) {
+			server := &http.Server{
+				Addr:    ":8080",
+				Handler: r,
+			}
+
+			lc.Append(fx.Hook{
+				OnStart: func(ctx context.Context) error {
+					log.Println("Server starting on :8080")
+					go server.ListenAndServe()
+					return nil
+				},
+				OnStop: func(ctx context.Context) error {
+					log.Println("Server shutting down...")
+					return server.Shutdown(ctx)
+				},
+			})
 		}),
 	)
 
